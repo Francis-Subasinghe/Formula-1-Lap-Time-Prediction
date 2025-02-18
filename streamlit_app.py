029fd394c648e36059fb96b4553a9b55baaf1547
Francis Subasinghe Arachchige Don <w1761877@westminster.ac.uk>
2025-02-18 18:45:35 +0000

    Updated API URL for Streamlit Deployment

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ sector3_time = st.sidebar.number_input("Sector 3 Time (s)", min_value=0.0, step=
 compound = st.sidebar.selectbox("Tyre Compound", ["Soft", "Medium", "Hard"])
 
 # FastAPI Endpoint
-API_URL = "http://127.0.0.1:8000/predict/"
+API_URL = "https://formula-1-lap-time-prediction.onrender.com"
 
 # Predict Button
 if st.sidebar.button("Predict Lap Time"):
